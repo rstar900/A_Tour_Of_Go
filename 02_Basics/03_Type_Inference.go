46dd73a69573d49e969c668d84e405c4ddfb6d6d
Rachit Garg <rachitgarg900@gmail.com>
2025-01-01 15:32:40 +0100

    Create 03_Type_Inference.go

---
+++ 02_Basics/03_Type_Inference.go
@@ -0,0 +1,10 @@
+https://go.dev/tour/basics/14
+
+package main
+
+import "fmt"
+
+func main() {
+	v := "LOL" // changed to String
+	fmt.Printf("v is of type %T\n with value of %v", v, v) // modified to print value as well
+}
