52afa2a815ba973ca4244d59c8d7fb530af45106
Rachit Garg <rachitgarg900@gmail.com>
2025-01-11 14:00:35 +0100

    Create 03_Exercise_Reader.go

---
+++ 05_Methods_and_Interfaces/03_Exercise_Reader.go
@@ -0,0 +1,40 @@
+// https://go.dev/tour/methods/22
+
+package main
+
+import (
+	"golang.org/x/tour/reader"
+	"fmt"
+)
+
+type MyReader struct{}
+
+// Error type to denote empty slice
+type EmptyBufferErr struct{}
+
+// Implement Error interface for EmptyBufferErr
+func (e EmptyBufferErr) Error() string {
+	return fmt.Sprint("No space in buffer to read!")
+}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (mr MyReader) Read(b []byte) (int, error) {
+	// Only give error when given slice is nil
+	if b == nil {
+		return 0, EmptyBufferErr{}
+	} 
+	
+	bytes := 0 // Bytes read
+	
+	// Read 'A' into all the available bytes of the slice
+	for i := range b {
+		b[i] = 'A'
+		bytes++
+	}
+	
+	return bytes, nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
